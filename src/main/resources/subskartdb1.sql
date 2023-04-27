f1ee75840b23d5e69d63fbd89395acc932f8dff7
Kiran baghel <114062163+Kiran-baghel2022@users.noreply.github.com>
2023-04-27 22:16:47 +0530

    sql file

---
+++ src/main/resources/subskartdb1.sql
@@ -0,0 +1,6 @@
+insert into subskartdb1.login values(1,'kiran@kiran.com',true,'$2a$10$c3W9CqVYKT5b/qlStIGn2ecqywRTp.d5c5cDfKsWGecRud3Kh1hzu','kiran');
+insert into subskartdb1.item values(1,'Tea',10);
+insert into subskartdb1.plan values(1,30,'#0 days');
+insert into subskartdb1.vender values(1,'kiran');
+insert into subskartdb1.item_plan_vender_mapping values(1,1,1,101);
+insert into subskartdb1.role values(1,'admin');
